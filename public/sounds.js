8c2afb188af2c8263be673cf6a0ea6b3a5a5c8ef
Douglas Fleischut <df@douglass-imac.lan>
2017-11-29 12:40:24 -0800

    more sounds

--- public/sounds.js
+++ public/sounds.js
@@ -1,63 +1,250 @@
 var sounds = [
   {
-  name: 'alligator',
+  name: 'clap',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/alligator-1.mp3'
+  path:'/sounds/playSounds/clap1.mp3'
 },
+{
+  name: 'snarey1',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/snarey1.mp3'
+},
+
+{
+  name: 'snarey2',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/snarey2.mp3'
+},
+{
+  name: 'bassy',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/bassy.mp3'
+},
+{
+  name: 'tip-toe',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/tip-toe.mp3'
+},
+{
+  name: 'sizzle',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/sizzle.mp3'
+},
+{
+  name: 'sharp',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/sharp.mp3'
+},
+{
+  name: 'xylo',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/xylo.mp3'
+},
+///////////////////////
+
+{
+  name: 'arc',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/arc.mp3'
+},
+{
+  name: 'bellish',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/bellish.mp3'
+},
+
+{
+  name: 'bird',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/bird.mp3'
+},
+{
+  name: 'boop',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/boop.mp3'
+},
+{
+  name: 'code',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/code.mp3'
+},
+{
+  name: 'critter',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/critter.mp3'
+},
+{
+  name: 'data',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/data.mp3'
+},
+{
+  name: 'elec',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/elec.mp3'
+},
+
+{
+  name: 'glowing',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/glowing.mp3'
+},
+{
+  name: 'interfere',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/interfere.mp3'
+},
+
+{
+  name: 'korltron',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/korltron.mp3'
+},
+{
+  name: 'metal',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/metal.mp3'
+},
+{
+  name: 'modvox',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/modvox.mp3'
+},
+{
+  name: 'oohahh',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/oohahh.mp3'
+},
+{
+  name: 'oops',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/oops.mp3'
+},
+{
+  name: 'phone',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/phone.mp3'
+},
+
+
+
+{
+  name: 'print',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/print.mp3'
+},
+{
+  name: 'pulse',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/pulse.mp3'
+},
+{
+  name: 'raspy',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/raspy.mp3'
+},
+{
+  name: 'ray',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/ray.mp3'
+},
+{
+  name: 'scanner',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/scanner.mp3'
+},
+{
+  name: 'wonk',
+  chosen: false,
+  bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
+  path:'/sounds/playSounds/wonk.mp3'
+},
+
+
+
+/////////////////////
 {
   name: 'bobcat',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/bobcat-1.mp3'
+  path:'/sounds/playSounds/bobcat-1.mp3'
 },
 {
   name: 'hummingbird',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/hb-short-5.mp3'
+  path:'/sounds/playSounds/hb-short-5.mp3'
 },
 {
   name: 'horse',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/horse-5.mp3'
+  path:'/sounds/playSounds/horse-5.mp3'
 },
 {
   name: 'monkeys',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/monkeys4.mp3'
+  path:'/sounds/playSounds/monkeys4.mp3'
 },
 {
   name: 'sea lion',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/sea-lion-3.mp3'
+  path:'/sounds/playSounds/sea-lion-3.mp3'
 },
 {
   name: 'gorilla',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/strange-gorilla-1.mp3'
+  path:'/sounds/playSounds/strange-gorilla-1.mp3'
 },
 {
   name: 'walnuts',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/walnuts5.mp3'
+  path:'/sounds/playSounds/walnuts5.mp3'
 },
 {
   name: 'wobby saw',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/wobb-sh-2.mp3'
+  path:'/sounds/playSounds/wobb-sh-2.mp3'
 },
 {
   name: 'woodpeckers',
   chosen: false,
   bgColor: '#'+Math.floor(Math.random()*16777215).toString(16),
-  path:'/sounds/wp-sh-2.mp3'
+  path:'/sounds/playSounds/wp-sh-2.mp3'
 }
 
 
